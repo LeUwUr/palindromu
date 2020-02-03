98a5fe0695e3fe902b2f1114fe795266f5912263
LeUwUr <60027910+LeUwUr@users.noreply.github.com>
2020-02-02 23:17:59 -0800

    Update pal.py

--- pal.py
+++ pal.py
@@ -3,7 +3,7 @@ frase = input("ingrese una frase: ") # No te ocupo explicar aqui.
 frase_sin_espacios = frase.replace(" ","") # A .replace se le ha indicado hacer una accion, la cual es eliminar los espacios si es que se le a añadido una frase, esto para que el programa detecte si es un palindromo.
 
 if frase_sin_espacios.islower() == False: # Ya que se eliminaron los espacios, se comprueba que el string sea solo en minusculas.
- frase_sin_espacios = frase_sin_espacios.islower() # La funcion .islower convierte el string a minusculas con el fin de que el programa se confunda al realizar la comprobacion.
+ frase_sin_espacios = frase_sin_espacios.islower() # La funcion .islower convierte el string a minusculas con el fin de que el programa no se confunda al realizar la comprobacion.
 
 frase_invertida = frase_sin_espacios[::-1] # Para invertir la frase se usa ::-1 para asignar a la nueva variable el string comenzando de la ultima posicion menos lo que nos dara una cadena al reves.
 
